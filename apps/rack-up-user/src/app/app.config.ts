462001f72ef77909c52dd2aaeec3abd9100c349e
LloydTDavies <35774168+LloydTDavies@users.noreply.github.com>
2024-08-26 21:04:29 +0100

    feature: Added firebase hosting (#1)
    * feature: Added firebase hosting
    
    * fix: Fixed linting

--- apps/rack-up-user/src/app/app.config.ts
+++ apps/rack-up-user/src/app/app.config.ts
@@ -1,10 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { FirebaseService } from '@rack-up/firebase';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: FirebaseService,
+      useClass: FirebaseService,
+    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideFirebaseApp(() =>
+      initializeApp(new FirebaseService().firebaseConfig)
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
---
+++ libs/shared/firebase/src/lib/firebase.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  readonly firebaseConfig = {
+    apiKey: "AIzaSyBtn6pQdyWsUlHdKCol6bZeD79VgifzWOA",
+    authDomain: "rackup-4a20e.firebaseapp.com",
+    projectId: "rackup-4a20e",
+    storageBucket: "rackup-4a20e.appspot.com",
+    messagingSenderId: "388070192203",
+    appId: "1:388070192203:web:e771dfb0ffc57d5c6cc9ce",
+    measurementId: "G-EHHB81M40Z"
+  };
+}
